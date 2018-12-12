bfce8d94665a6dedbc0859196794abdc599597ce
Chiorean Dan <chiorean.dan12@gmail.com>
2018-12-12 23:00:05 +0200

    Fix column name and add routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_200104) do
+ActiveRecord::Schema.define(version: 2018_12_12_205620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_12_12_200104) do
     t.bigint "user_id"
     t.string "topic"
     t.string "name"
-    t.string "type"
+    t.string "var_type"
     t.string "room"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
