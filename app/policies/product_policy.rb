7701f47d8185db5e35959ccbe911555352a48dd6
Chiorean Dan <chiorean.dan12@gmail.com>
2019-04-11 21:54:26 +0300

    change product policy

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class ProductPolicy < ApplicationPolicy
-  # def show?
-  #   user.admin?
-  # end
+  def show?
+    user.admin?
+  end
 
-  # def create?
-  #   show?
-  # end
+  def create?
+    show?
+  end
 
-  # def update?
-  #   show?
-  # end
+  def update?
+    show?
+  end
 
   def as_json
     {
